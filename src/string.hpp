e81747349888731b7fb68c2f8332ee03d2826a32
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-02 23:37:03 -0700

    Update string.hpp

--- src/string.hpp
+++ src/string.hpp
@@ -1,4 +1,4 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
 
@@ -7,8 +7,7 @@ class String
 	char* buf;
 	
 public:
-
-static int strlen(const char* s);
+	static int strlen(const char* s);
 	static char* strdup(const char* s);
 	static char* strcpy(char* dest, const char* src);
 	static char* strncpy(char* dest, const char* src, int n);
@@ -17,11 +16,9 @@ static int strlen(const char* s);
 	static int strcmp(const char* left, const char* right);
 	static int strncmp(const char* left, const char* right, int n);
 	static void reverse_cpy(char* dest, const char* src);
-	static char* strchr(char* str, char c);
+	static const char* strchr(const char* str, char c);
 	static  char* strstr(char* haystack, const char* needle);
 	static const char* strstr(const char* haystack, const char* needle);
-
-
 
 	explicit String(const char* s = "");
 	String(const String& s);
@@ -51,9 +48,11 @@ static int strlen(const char* s);
 	void read(istream& in);
 
 	~String();
+	bool in_bounds(int i);
 
 private:
-	bool in_bounds(int i);
+	//bool in_bounds(int i);
+
 };
 
 ostream& operator << (ostream& out, const String& s);
